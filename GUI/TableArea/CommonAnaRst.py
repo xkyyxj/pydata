d8846982bb43b1cc8cc02914fc8e58c78069d272
Cassiopeia <wang_qingchun@outlook.com>
2020-09-29 20:52:34 +0800

    进一步修正图形界面问题

---
+++ GUI/TableArea/CommonAnaRst.py
@@ -0,0 +1,9 @@
+class CommonAnaRst:
+    def __init__(self):
+        self.data = []
+        self.db_columns = []
+        self.display_head = []
+        self.db_table_name = []
+        self.table_meta = []
+
+
--- GUI/main_windows.py
+++ GUI/main_windows.py
@@ -3,12 +3,36 @@ from PySide2 import QtCore, QtWidgets, QtGui
 from PySide2.QtCore import QDir
 from PySide2.QtWidgets import QSizePolicy, QTreeView, QFileSystemModel
 
+from Data.DataCenter import DataCenter
+from GUI.treeItem.Category import Category
 from GUI.treeItem.CategoryTreeModel import CategoryTreeModel
 from ui_config.main_window import Ui_MainWindow
 
 
 def tree_node_selected(selected, un_selected):
     print("hehedada ahahahhahahhahahah")
+    index = selected.indexes().first();
+    var_data = index.data(Category.IDRole)
+    pk_field = var_data.toString();
+
+    # 查询一下是哪张表
+    table_name = None
+    pk_table_meta = None
+    is_redis = False
+    instance = DataCenter.get_instance()
+    query_table_name = "select table_name, ana_category.pk_tablemeta, is_redis from ana_category join table_meta " \
+                       "on ana_category.pk_tablemeta=table_meta.pk_tablemeta where pk_category='"
+    query_table_name = query_table_name + pk_field + "'"
+    ret_val = instance.common_query(query_table_name)
+    if len(ret_val) > 0:
+        table_name = ret_val[0][0]
+        pk_table_meta = ret_val[0][1]
+        is_redis = ret_val[0][2] == 'Y'
+
+    if not is_redis:
+        pass
+
+    # anaRstTypeSelect(tableName.toStdString(), pk_tablemeta.toStdString());
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -28,17 +52,6 @@ class MainWindow(QtWidgets.QMainWindow):
         # tree.setModel(model)
 
         tree_model = CategoryTreeModel()
-        # self.tree_view = QtWidgets.QTreeView(self)
-        # self.tree_view.setModel(tree_model)
-        # self.tree_view.setFixedWidth(200)
-        # self.tree_view.setFixedHeight(600)
-        # self.tree_view.selectionModel().selectionChanged.connect(tree_node_selected)
-        # sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        # sizePolicy.setHorizontalStretch(1)
-        # sizePolicy.setVerticalStretch(0)
-        # sizePolicy.setHeightForWidth(self.tree_view.sizePolicy().hasHeightForWidth())
-        # self.tree_view.setSizePolicy(sizePolicy)
-        # self.ui.treeView.setSelectionMode(QtWidgets.QAbstractItemView.)
         self.ui.treeView.setModel(tree_model)
         self.ui.treeView.selectionModel().selectionChanged.connect(tree_node_selected)
 
